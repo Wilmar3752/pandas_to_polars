22e57dc47a3675327bef8ad38e1b157334a284af
wilmar mac <wilmar.sepulveda@correounivalle.edu.co>
2024-07-27 19:33:58 -0500

    readme and bash scripting modified

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pandas_to_polars
 Migrating from pandas to polars
+# 1 Generate the benchmark data
+
+```bash
+sh generate_benchmark_data.sh
+```
--- generate_benckmark_data.sh
+++ generate_benckmark_data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Ruta al script Python que genera el archivo large_dataset.csv
+python_script="src/generate_large_dataset.py"
+
+# Ejecuta el script Python para generar el archivo large_dataset.csv
+echo "Generando el archivo large_dataset.csv..."
+python "$python_script"
+
 # Nombre base del archivo
 filename="data/large_dataset"
 # Número de veces que se quiere duplicar el tamaño del archivo
